f694863d77dec2a8b17ca6820676ac32e0a87788
Roani Pires da Silva <roani.silva@gazin.com.br>
2023-10-04 10:13:44 -0300

    update

---
+++ src/Livraria/Livraria.Api/Controllers/CategoriasController.cs
@@ -0,0 +1,95 @@
+﻿using Livraria.Domain.Entity;
+using Livraria.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Livraria.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly LivrariaContext _context;
+
+    public CategoriasController(LivrariaContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+    {
+        return await _context.Categorias.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Categoria>> GetCategoria(int id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+
+        if (categoria == null)
+        {
+            return NotFound();
+        }
+
+        return categoria;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+    {
+        _context.Categorias.Add(categoria);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetCategoria", new { id = categoria.CategoriaId }, categoria);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+    {
+        if (id != categoria.CategoriaId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!CategoriaExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCategoria(int id)
+    {
+        var categoria = await _context.Categorias.FindAsync(id);
+        if (categoria == null)
+        {
+            return NotFound();
+        }
+
+        _context.Categorias.Remove(categoria);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool CategoriaExists(int id)
+    {
+        return _context.Categorias.Any(e => e.CategoriaId == id);
+    }
+}
---
+++ src/Livraria/Livraria.Infrastructure.Data/LivrariaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Livraria.Domain.Entity;
+
+namespace Livraria.Infrastructure.Data;
+
+public class LivrariaContext : DbContext
+{
+    public LivrariaContext(DbContextOptions<LivrariaContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Autor> Autores { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Livro> Livros { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Autor>().ToTable("autores");
+        modelBuilder.Entity<Categoria>().ToTable("categorias");
+        modelBuilder.Entity<Livro>().ToTable("livros");
+    }
+}
